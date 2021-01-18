04eb6e024f81fe66e4f01c2874abd4e1f079eaaa
gwcew <git.matvey@yandex.ru>
2021-01-18 11:43:35 +0300

    ADD: something new

--- site/pages/index.js
+++ site/pages/index.js
@@ -1,15 +1,26 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+import AppBar from '@material-ui/core/AppBar'
+import Container from '@material-ui/core/Container'
+import ToolBar from '@material-ui/core/Toolbar'
+import IconButton from '@material-ui/core/IconButton'
+import Button from '@material-ui/core/Button'
+
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Magazine</title>
+        <title>Cars</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div>Hello world</div>
+      <Container>
+        <AppBar position="static">
+          <ToolBar>
+          </ToolBar>
+        </AppBar>
+      </Container>
     </div>
   )
 }
